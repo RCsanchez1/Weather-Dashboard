3c46109e9a884ddde586e5b335e7ea91c5c4059f
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-07-03 21:38:21 -0600

    added apikey

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-let cities = ["Boston", "Philadelphia", "Detroit", "San Francisco"];
-let apiKey = "c94ac49bcd423ef700d020797840e0c4";
-let lat = "latitude";
-let lon = "longitude";
-let uvIndex = (lat + lon)
+// store the value of the input
+let city = $("#searchTerm").val();
+// store api key
+const apiKey = "&appid=afaa8eea1769b4359fd8e07b2efcefbd";
 
-cities.forEach(function (city, index, originalArr) {
-    renderButtons(city);
+let date = new Date();
 
-    if (index === originalArr.length - 1) {
-        displayWeatherInfo(city);
-    }
-})
-
-function displayWeatherInfo(city) {
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial`;
-
-    // let queryUV = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-    $.get(queryURL).then(function (response) {
-        // let unIndex = response.coord.lon.lat;
-        let lon = response.coord.lon;
-        let lat = response.coord.lat;
-        let queryUV = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-       
+$("#searchTerm").keypress(function(event) { 
+	
+	if (event.keyCode === 13) { 
+		event.preventDefault();
+		$("#searchBtn").click(); 
+	} 
+});
